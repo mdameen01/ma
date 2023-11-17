7c366d020f2f77f9fe6d778712748ea91ce8c199
unknown <muhannadkhan66@gmail.com>
2023-11-17 12:21:57 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <p style={{color:"purple",fontWeight:"bold"}}>FATAFAT Footer</p>
+        </div>
+  )
+}
+
+export default Footer
